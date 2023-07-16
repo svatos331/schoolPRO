0526a443a535156d8d2620ae8f86ea584bc48f56
kira <k-korni1ov@yandex.ru>
2023-07-16 17:42:34 +0400

    feat: done Signpage

--- app/src/user.InterfaceLayer/Libraries/KirillKornilov.library/Widgets/LogInForm.widget/index.tsx
+++ app/src/user.InterfaceLayer/Libraries/KirillKornilov.library/Widgets/LogInForm.widget/index.tsx
@@ -13,26 +13,30 @@ import "./styled/style.css";
 
 export const LogInForm =()=>{
 	return(
-		<div className="wrapper">
-			<h4>Вход в аккаунт</h4>
-			<input type="text" placeholder="E-mail" />
-			<input type="text" placeholder="Пароль" />
-			<Button text="Войти" color="#29A19C" icon={false}/>
-			<p>Ещё нет аккаунта? <a href="#">Регистрация</a></p>
-			<div className="division-box">
-				<hr />
-				<p>или</p>
-				<hr />
-			</div>
-			<div className="media-box">
-				<Media logo={require("../../assets/icons/twitter.png")} link="twitter.com"/>
-			<Media logo={require("../../assets/icons/facebook.webp")} link="facebook.com"/>
-			
-				</div> 	 
-				{/* KirillKornilov.library/assets/icons/avatar.png
-			  KirillKornilov.library/Widgets/LogInForm.widget/index.tsx
-					 KirillKornilov.library/UI_KIT/Atoms/Media.Atom/index.tsx */}
+		<div className="body">
+			<div className="wrapper">
+				
+				<h4>Вход в аккаунт</h4>
+				<input type="text" placeholder="E-mail" />
+				<input type="text" placeholder="Пароль" />
+				<Button text="Войти" color="#29A19C" icon={false}/>
+				<p>Ещё нет аккаунта? <a href="#">Регистрация</a></p>
+				<div className="division-box">
+					<hr />
+					<p>или</p>
+					<hr />
+				</div>
+				<div className="media-box">
+					<Media logo={require("../../assets/icons/twitter.png")} link="twitter.com"/>
+				<Media logo={require("../../assets/icons/facebook.webp")} link="facebook.com"/>
+				
+					</div> 	 
+					{/* KirillKornilov.library/assets/icons/avatar.png
+				KirillKornilov.library/Widgets/LogInForm.widget/index.tsx
+						KirillKornilov.library/UI_KIT/Atoms/Media.Atom/index.tsx */}
 
+
+			</div>
 		</div>
 	);
 };
