96ca4f1ab0d7f1ef7e413f92b4a83d044f44a7b5
kira <k-korni1ov@yandex.ru>
2023-07-17 13:57:41 +0400

    feat: half mainPage

--- app/src/user.InterfaceLayer/Libraries/KirillKornilov.library/UI_KIT/Molecules/CardDataOptions.molecule/index.tsx
+++ app/src/user.InterfaceLayer/Libraries/KirillKornilov.library/UI_KIT/Molecules/CardDataOptions.molecule/index.tsx
@@ -1,3 +1,24 @@
-const test = {};
+import { FC } from "react";
 
-export default test;
+import {ICardData} from "../../Atoms/CardData.Atom";
+
+
+const CardDataOptions: FC<ICardData> = ({title, children}) => {
+
+    return (
+
+        <div className="column-card">
+                <div className="title-box">
+                <p className="title">{title}</p>
+                <button>•••</button>
+                </div>
+
+                <div className="children">
+                    {children}
+                </div>
+        </div>
+        
+    );
+};
+
+export default CardDataOptions;
--- app/src/user.InterfaceLayer/Pages/KirillKornilov.pages/Main.page/index.tsx
+++ app/src/user.InterfaceLayer/Pages/KirillKornilov.pages/Main.page/index.tsx
@@ -2,20 +2,64 @@ import React, { FC } from "react";
 import ComponentSideBarMain from "user.InterfaceLayer/Components/KirillKornilov.components.bll/SideBarDataMain.component.bll";
 //import Component1 from "../../../Components/KirillKornilov.components.bll/Components1.component.bll";
 // import { Component1 } from "test-lib";
+import Task from "user.InterfaceLayer/Libraries/KirillKornilov.library/UI_KIT/Molecules/Task.molecule";
 import MainHeader from "user.InterfaceLayer/Components/KirillKornilov.components.bll/HeaderMain.component.bll";
+import Card from "user.InterfaceLayer/Libraries/KirillKornilov.library/UI_KIT/Atoms/Card.Atom";
+import CardData from "user.InterfaceLayer/Libraries/KirillKornilov.library/UI_KIT/Atoms/CardData.Atom";
+import CardDataOptions from "user.InterfaceLayer/Libraries/KirillKornilov.library/UI_KIT/Molecules/CardDataOptions.molecule";
 
 import "./style.css";
 
 const MainPage: FC = () => {
-	// eslint-disable-next-line no-console
-	// console.log(Component1);
 
 	return 	(
 
 			<>
-				<ComponentSideBarMain/>
-				<div id='main-page'><MainHeader/></div>
-				
+			<ComponentSideBarMain/>
+			<div id='main-page'>
+				<MainHeader/>
+				<div className='columns'>
+					<div className='left-column'>
+					<>
+						<Card>
+						<>
+							<CardDataOptions title="Активные задачи">
+								<div className="tasks-list">
+									<Task text="Приготовить вкусный ужин" completed={false}/>
+									<Task text="Устранить засор в раковине" completed={false}/>
+									<Task text="Стирка белого белья" completed={false}/>
+									<Task text="Разморозить холодильник" completed={false}/>
+								</div>
+							</CardDataOptions>
+							<CardData title="Заверщённые задачи">
+								<div className="tasks-list">
+									<Task text="Полить цветы" completed={true}/>
+									<Task text="Вызвать сантехника" completed={true}/>
+								</div>
+							</CardData>
+
+							
+						</>
+						</Card>
+					</>
+					</div>
+					<div className="right-column">
+						<Card>
+							<>
+							<CardData title="Факт дня">
+								<>
+									<p style={{margin:0}}>
+										Человек, который просыпается в 6 утра, по статистике,
+										закрывает все задачи к 18:30 вечера. 
+										Попробуем также :^| ?
+									</p>
+								</>
+							</CardData>
+							</>
+						</Card>
+					</div>
+				</div>
+			</div>
 			</>
 
 			);
