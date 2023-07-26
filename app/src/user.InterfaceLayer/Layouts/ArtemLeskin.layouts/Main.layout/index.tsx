d8fdeb7f94c349d685644a7649797d030e070af4
ArtemSPN <87538803+ArtemSPN@users.noreply.github.com>
2023-07-27 00:43:20 +0400

    add mobile version

--- app/src/user.InterfaceLayer/Layouts/ArtemLeskin.layouts/Main.layout/index.tsx
+++ app/src/user.InterfaceLayer/Layouts/ArtemLeskin.layouts/Main.layout/index.tsx
@@ -1,16 +1,26 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { Header } from "user.InterfaceLayer/Libraries/ArtemLeskin.library/Widgets/Header";
 import { Outlet } from "react-router-dom";
+import Sidebar from "user.InterfaceLayer/Libraries/ArtemLeskin.library/UI_KIT/Molecules/Sidebar";
 
-import cls from "./MainLayout.module.css";
+import * as ST from "./style/style";
 
-const MainLayout: FC = () => (
-	<div className={cls.app}>
-		<div className={cls.container}>
-            <Header/>
-        </div>
+const MainLayout: FC = () => {
+    const [isActive, setIsActive] = useState(false);
+
+    const changeActive = () => {
+        setIsActive(!isActive);
+    };
+
+    return (
+        <ST.App>
+		<ST.Container>
+            <Header changeActive={changeActive} isActive={!isActive}/>
+        </ST.Container>
+        <Sidebar isActive={isActive}/>
         <Outlet />
-	</div>
-);
+	</ST.App>
+    );
+};
 
 export default MainLayout;
--- app/src/user.InterfaceLayer/Libraries/ArtemLeskin.library/Widgets/InfoBlock/style/style.ts
+++ app/src/user.InterfaceLayer/Libraries/ArtemLeskin.library/Widgets/InfoBlock/style/style.ts
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import RatingComponent from "user.InterfaceLayer/Libraries/ArtemLeskin.library/UI_KIT/Atoms/Rating";
+import Text from "user.InterfaceLayer/Libraries/ArtemLeskin.library/UI_KIT/Atoms/Text";
 
 export const Block = styled.div`
     width: 100%;
@@ -19,6 +20,10 @@ export const Info = styled.div`
     top: 60%;
     width: 45%;
     left: 50px;
+    @media (max-width: 426px) {
+        top: 50%;
+        width: 85%;
+    }
 `;
 export const Img = styled.img`
     width: 80%;
@@ -30,33 +35,44 @@ export const HeaderInfo = styled.div`
     align-items: center;
     padding-top: 10px;
     width: 100%;
+    flex-wrap: wrap;
 `;
 
 export const Rating = styled(RatingComponent)`
     font-weight: bold;
     border-radius: 5px;
 `;
-export const Txt = styled.div`
+export const Txt = styled(Text)`
     font-size: 20px;
     font-weight: bold;
     color: rgb(255, 255, 255);
-`;
-export const TxtArr = styled.div`
-    font-size: 14px;
-    font-weight: bold;
-    color: rgb(255, 255, 255);
+
+    @media (max-width: 426px) {
+        width: 100%;
+        height: 20px;
+    }
 `;
 
 export const Description = styled.div`
     margin-top: 15px;
     margin-left: 70px;
+    @media (max-width: 426px) {
+        margin-top: 100px;
+        margin-left: 10px;
+    }
 `;
 
 export const TxtContainer = styled.div`
     display: flex;
     height: 40px;
     width: 100%;
+    flex-wrap: wrap;
     justify-content: space-between;
     margin-left: 80px;
     align-items: center;
+    @media (min-width: 426px) {
+        flex-direction: column;
+        width: 100%;
+        height: 100px;
+    }
 `;
