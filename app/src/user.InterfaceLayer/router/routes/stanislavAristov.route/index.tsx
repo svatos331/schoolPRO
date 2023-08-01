e71f6577303819075dbb4243965f373bc55590e8
COCKTAIL <aristovstas2004@gmail.com>
2023-08-01 18:20:56 +0400

    fix routes to domain routes

--- app/src/user.InterfaceLayer/router/routes/stanislavAristov.route/index.tsx
+++ app/src/user.InterfaceLayer/router/routes/stanislavAristov.route/index.tsx
@@ -10,11 +10,11 @@ import AuthWidget from "../../../Libraries/payments.library/Widgets/authWidget";
 import WelcomeAuth from "../../../Libraries/payments.library/Widgets/welcomeAuth";
 import AfterAuthLayout from "../../../Layouts/stanislavAristov.layouts/AfterAuth.layout";
 import Colors from "../../../constants/colors/index";
-import ProfilePage from "../../../Pages/AristovStanislav.pages/Profile.page";
-import MainPage from "../../../Pages/AristovStanislav.pages/Main.page";
-
 const CardsPage = lazy(
 	() => import("../../../Pages/AristovStanislav.pages/Cards.page")
+);
+const ProfilePage = lazy(
+	() => import("../../../Pages/AristovStanislav.pages/Profile.page")
 );
 const PaymentPage = lazy(
 	() => import("../../../Pages/AristovStanislav.pages/Payment.page")
@@ -26,86 +26,85 @@ const HomePage = lazy(
 	() => import("../../../Pages/AristovStanislav.pages/Home.page")
 );
 const stanislavAristovRoutes: RouteObject[] = [
+	// {
+	// 	// path: routesPaths.STANISLAV_ARISTOV,
+	// 	// element: <BeforeAuthLayout />,
+	// 	// errorElement: <Error />,
+	// 	// children: [
+	// 	// 	// {
+	// 	// 	// 	path: `/${stanislavAristovRoutesPaths.AUTH}`,
+	// 	// 	// 	element: (
+	// 	// 	// 		<Suspense fallback={<Loader />}>
+	// 	// 	// 			<WelcomeAuth />
+	// 	// 	// 		</Suspense>
+	// 	// 	// 	),
+	// 	// 	// 	errorElement: <Error />,
+	// 	// 	// },
+	// 	// 	// {
+	// 	// 	// 	path: stanislavAristovRoutesPaths.SIGNIN,
+	// 	// 	// 	index: true,
+	// 	// 	// 	element: (
+	// 	// 	// 		<Suspense fallback={<Loader />}>
+	// 	// 	// 			<AuthWidget title={"Sign in"} />
+	// 	// 	// 		</Suspense>
+	// 	// 	// 	),
+	// 	// 	// 	errorElement: <Error />,
+	// 	// 	// },
+	// 	// 	// {
+	// 	// 	// 	path: stanislavAristovRoutesPaths.SIGNUP,
+	// 	// 	// 	index: true,
+	// 	// 	// 	element: (
+	// 	// 	// 		<Suspense fallback={<Loader />}>
+	// 	// 	// 			<AuthWidget title={"Sign up"} />
+	// 	// 	// 		</Suspense>
+	// 	// 	// 	),
+	// 	// 	// 	errorElement: <Error />,
+	// 	// 	// },
+	// 	// 	{
+	// 	// 		path: routesPaths.NOT_FOUND,
+	// 	// 		element: <div>страницы нет</div>,
+	// 	// 	},
+	// 	// ],
+	// },
+	// {
+	// 	path: `/${stanislavAristovRoutesPaths.AUTH}`,
+	// 	element: <AfterAuthLayout />,
+	// 	errorElement: <Error />,
+	// 	children: [
+	// 		{
+	// 			index: true,
+	// 			path: `${stanislavAristovRoutesPaths.OTP}`,
+	// 			element: (
+	// 				<Suspense fallback={<Loader />}>
+	// 					<OTPPage />
+	// 				</Suspense>
+	// 			),
+	// 		},
+	// 	],
+	// },
+	// {
+	// 	path: `/${stanislavAristovRoutesPaths.AUTH}/${stanislavAristovRoutesPaths.CREATINPROFILE}`,
+	// 	element: <AfterAuthLayout />,
+	// 	errorElement: <Error />,
+	// 	children: [
+	// 		{
+	// 			index: true,
+	// 			path: `/${stanislavAristovRoutesPaths.AUTH}/${stanislavAristovRoutesPaths.CREATINPROFILE}`,
+	// 			element: (
+	// 				<Suspense fallback={<Loader />}>
+	// 					<Error />
+	// 				</Suspense>
+	// 			),
+	// 		},
+	// 	],
+	// },
 	{
-		path: stanislavAristovRoutesPaths.AUTH,
-		element: <BeforeAuthLayout />,
-		errorElement: <Error />,
-		children: [
-			{
-				path: `/${stanislavAristovRoutesPaths.AUTH}`,
-				element: (
-					<Suspense fallback={<Loader />}>
-						<WelcomeAuth />
-					</Suspense>
-				),
-				errorElement: <Error />,
-			},
-			{
-				path: stanislavAristovRoutesPaths.SIGNIN,
-				index: true,
-				element: (
-					<Suspense fallback={<Loader />}>
-						<AuthWidget title={"Sign in"} />
-					</Suspense>
-				),
-				errorElement: <Error />,
-			},
-			{
-				path: stanislavAristovRoutesPaths.SIGNUP,
-				index: true,
-				element: (
-					<Suspense fallback={<Loader />}>
-						<AuthWidget title={"Sign up"} />
-					</Suspense>
-				),
-				errorElement: <Error />,
-			},
-
-			{
-				path: routesPaths.NOT_FOUND,
-				element: <div>страницы нет</div>,
-			},
-		],
-	},
-	{
-		path: `/${stanislavAristovRoutesPaths.AUTH}`,
-		element: <AfterAuthLayout />,
-		errorElement: <Error />,
-		children: [
-			{
-				index: true,
-				path: `${stanislavAristovRoutesPaths.OTP}`,
-				element: (
-					<Suspense fallback={<Loader />}>
-						<OTPPage />
-					</Suspense>
-				),
-			},
-		],
-	},
-	{
-		path: `/${stanislavAristovRoutesPaths.AUTH}/${stanislavAristovRoutesPaths.CREATINPROFILE}`,
-		element: <AfterAuthLayout />,
-		errorElement: <Error />,
-		children: [
-			{
-				index: true,
-				path: `/${stanislavAristovRoutesPaths.AUTH}/${stanislavAristovRoutesPaths.CREATINPROFILE}`,
-				element: (
-					<Suspense fallback={<Loader />}>
-						<Error />
-					</Suspense>
-				),
-			},
-		],
-	},
-	{
-		path: routesPaths.MAIN,
+		path: routesPaths.STANISLAV_ARISTOV,
 		element: <AfterAuthLayout bgc={Colors.WHITE} />,
 		errorElement: <Error />,
 		children: [
 			{
-				path: `/${routesPaths.MAIN}`,
+				// path: `/${routesPaths.STANISLAV_ARISTOV}`,
 				element: (
 					<Suspense fallback={<Loader />}>
 						<HomePage />
@@ -114,7 +113,7 @@ const stanislavAristovRoutes: RouteObject[] = [
 				index: true,
 			},
 			{
-				path: `/${routesPaths.CARDS}`,
+				path: `/${stanislavAristovRoutesPaths.CARDS}`,
 				element: (
 					<Suspense fallback={<Loader />}>
 						<CardsPage />
@@ -123,7 +122,7 @@ const stanislavAristovRoutes: RouteObject[] = [
 				index: true,
 			},
 			{
-				path: `/${routesPaths.PAYMENT}`,
+				path: `/${stanislavAristovRoutesPaths.PAYMENT}`,
 				element: (
 					<Suspense fallback={<Loader />}>
 						<PaymentPage />
@@ -132,7 +131,7 @@ const stanislavAristovRoutes: RouteObject[] = [
 				index: true,
 			},
 			{
-				path: `/${routesPaths.PROFILE}`,
+				path: `/${stanislavAristovRoutesPaths.PROFILE}`,
 				element: (
 					<Suspense fallback={<Loader />}>
 						<ProfilePage />
@@ -140,25 +139,9 @@ const stanislavAristovRoutes: RouteObject[] = [
 				),
 				index: true,
 			},
-			{
-				path: "/main",
-				element: (
-					<Suspense fallback={<Loader />}>
-						<MainPage />
-					</Suspense>
-				),
-				index: true,
-			},
-			{
-				path: routesPaths.NOT_FOUND,
-				element: <div>страницы нет</div>,
-			},
+			{ path: routesPaths.NOT_FOUND, element: <div>страницы нет</div> },
 		],
 	},
-	{
-		path: routesPaths.NOT_FOUND,
-		element: <div>страницы нет</div>,
-	},
+	{ path: routesPaths.NOT_FOUND, element: <div>страницы нет</div> },
 ];
-
 export default stanislavAristovRoutes;
