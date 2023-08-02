f6b2a767c10e753f9496540d274ad733964a8428
COCKTAIL <aristovstas2004@gmail.com>
2023-08-02 19:51:53 +0400

    finish

--- app/src/user.InterfaceLayer/Libraries/stanislavAristov.library/Widgets/cardsBlock/type/index.ts
+++ app/src/user.InterfaceLayer/Libraries/stanislavAristov.library/Widgets/cardsBlock/type/index.ts
@@ -1,7 +1,6 @@
 import { UseQuery } from "@reduxjs/toolkit/dist/query/react/buildHooks";
 import { QueryDefinition } from "@reduxjs/toolkit/query";
 
-import { IUserDTO } from "../../../../../../business.InterfaceLayer/store/shared/entities/stanislavAristov.entities/payments.entity/services/dto/from.dto";
 import { ICardDTO } from "../../../../../../business.InterfaceLayer/store/shared/entities/stanislavAristov.entities/cards.entity/services/dto/from.dto";
 
 export type ICardsBlockProps = {
